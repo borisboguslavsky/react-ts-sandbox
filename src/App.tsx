674586865a6ddb1d30be29c55c9717ebefa742bc
Boris Boguslavsky <boris@boguslavsky.design>
2023-03-26 20:43:24 -0500

    solve FontTree issues, make FontTree controlled, revert Grid to stable version in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/system";
-import Grid from "@mui/material/Unstable_Grid2";
+// import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { Grid } from "@mui/material";
 
 import Card from "./components/Card";
 
@@ -20,89 +21,113 @@ function App() {
 	return (
 		<Box sx={{ flexGrow: 1, maxWidth: "1200px", padding: "36px" }}>
 			<Grid container spacing={2}>
-				<Card
-					title="Tree Select"
-					description="A tree view component with selectable nodes depicting a font tree with font families and sub-fonts"
-				>
-					<FontTree />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Tree Select"
+						description="A tree view component with selectable nodes depicting a font tree with font families and sub-fonts"
+					>
+						<FontTree />
+					</Card>
+				</Grid>
 
-				<Card
-					title="XState Vending Machine"
-					description="A state machine example using the XState library."
-				>
-					<VendingMachine />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="XState Vending Machine"
+						description="A state machine example using the XState library."
+					>
+						<VendingMachine />
+					</Card>
+				</Grid>
 
-				<Card
-					title="React-Hook-Form"
-					description="A component that uses Material UI form elements and the react-hook-form library to control the overall form."
-				>
-					<ReactHookForm />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="React-Hook-Form"
+						description="A component that uses Material UI form elements and the react-hook-form library to control the overall form."
+					>
+						<ReactHookForm />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Delay"
-					description="A component that uses a promise to display an entered message after a specified delay (in ms)."
-				>
-					<Delay />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Delay"
+						description="A component that uses a promise to display an entered message after a specified delay (in ms)."
+					>
+						<Delay />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Example: useContext()"
-					description="A component with a nested child component that utilizes the useContext() hook to access/update app-wide state via a custom Provider component."
-				>
-					<UseContext />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Example: useContext()"
+						description="A component with a nested child component that utilizes the useContext() hook to access/update app-wide state via a custom Provider component."
+					>
+						<UseContext />
+					</Card>
+				</Grid>
 
-				<Card
-					title="File Tree"
-					description="A file tree component that rescursively renders a series of nested unordered lists that represent a file structure."
-				>
-					<Tree />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="File Tree"
+						description="A file tree component that rescursively renders a series of nested unordered lists that represent a file structure."
+					>
+						<Tree />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Mock Login"
-					description="A login form with basic validation that sends a request to https://jsonplaceholder.typicode.com/ if the form is valid and outputs the response in the textarea below."
-				>
-					<Login />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Mock Login"
+						description="A login form with basic validation that sends a request to https://jsonplaceholder.typicode.com/ if the form is valid and outputs the response in the textarea below."
+					>
+						<Login />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Example: useReducer()"
-					description="A series of text input fields that utilize the useReducer() hook to execute actions across all of them."
-				>
-					<ReducerExample />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Example: useReducer()"
+						description="A series of text input fields that utilize the useReducer() hook to execute actions across all of them."
+					>
+						<ReducerExample />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Stopwatch"
-					description="A basic stopwatch component where users can pause/resume the timer and record/clear lap times."
-				>
-					<Stopwatch />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Stopwatch"
+						description="A basic stopwatch component where users can pause/resume the timer and record/clear lap times."
+					>
+						<Stopwatch />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Todo List"
-					description="A basic ToDo list component where users can add and remove items to the list."
-				>
-					<TodoList />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Todo List"
+						description="A basic ToDo list component where users can add and remove items to the list."
+					>
+						<TodoList />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Counter"
-					description="A counter component where users can increment and decrement the counter by the entered value."
-				>
-					<Counter />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Counter"
+						description="A counter component where users can increment and decrement the counter by the entered value."
+					>
+						<Counter />
+					</Card>
+				</Grid>
 
-				<Card
-					title="Calculator"
-					description="A basic calculator app where users can add, subtract, multiply, and divide two numbers."
-				>
-					<Calculator />
-				</Card>
+				<Grid item xs={12} md={6} lg={4}>
+					<Card
+						title="Calculator"
+						description="A basic calculator app where users can add, subtract, multiply, and divide two numbers."
+					>
+						<Calculator />
+					</Card>
+				</Grid>
 			</Grid>
 		</Box>
 	);
--- src/components/FontTree/FontTree.tsx
+++ src/components/FontTree/FontTree.tsx
@@ -1,18 +1,80 @@
+import { useState } from "react";
 import { placeholderFonts } from "./FontTreeData";
 
 import TreeView from "@mui/lab/TreeView";
 import TreeItem from "@mui/lab/TreeItem";
 
-const data = placeholderFonts;
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Box, Button } from "@mui/material";
 
 export const FontTree = () => {
+	const [expanded, setExpanded] = useState<string[]>([]);
+	const [selected, setSelected] = useState<string[]>([]);
+
+	const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+		setExpanded(nodeIds);
+	};
+
+	const handleSelect = (event: React.SyntheticEvent, nodeIds: string[]) => {
+		setSelected(nodeIds);
+	};
+
+	const handleExpandClick = () => {
+		setExpanded((oldExpanded) =>
+			oldExpanded.length === 0
+				? placeholderFonts.children.map((fontFamily, i) => i.toString())
+				: []
+		);
+	};
+
+	const handleSelectClick = () => {
+		setSelected((oldSelected) =>
+			oldSelected.length === 0
+				? placeholderFonts.children.map((fontFamily, i) => i.toString())
+				: []
+		);
+	};
 	return (
-		<TreeView>
-			{placeholderFonts.children.map((fontFamily, i) => {
-				return fontFamily.children.map((font, j) => {
-					return <TreeItem nodeId={`${i}_${j}`} label={font.label} />;
-				});
-			})}
-		</TreeView>
+		<>
+			<TreeView
+				aria-label="controlled"
+				defaultCollapseIcon={<ExpandMoreIcon />}
+				defaultExpandIcon={<ChevronRightIcon />}
+				expanded={expanded}
+				selected={selected}
+				onNodeToggle={handleToggle}
+				onNodeSelect={handleSelect}
+				multiSelect
+			>
+				{placeholderFonts.children.map((fontFamily, i) => {
+					return (
+						<TreeItem
+							key={`fontTreeFamily_${i}`}
+							nodeId={`${i}`}
+							label={fontFamily.label}
+						>
+							{fontFamily.children.map((font, j) => {
+								return (
+									<TreeItem
+										key={`fontTreeSubFont_${i}_${j}`}
+										nodeId={`${i}_${j}`}
+										label={font.label}
+									/>
+								);
+							})}
+						</TreeItem>
+					);
+				})}
+			</TreeView>
+			<Box sx={{ mb: 1 }}>
+				<Button onClick={handleExpandClick}>
+					{expanded.length === 0 ? "Expand all" : "Collapse all"}
+				</Button>
+				<Button onClick={handleSelectClick}>
+					{selected.length === 0 ? "Select all" : "Unselect all"}
+				</Button>
+			</Box>
+		</>
 	);
 };
